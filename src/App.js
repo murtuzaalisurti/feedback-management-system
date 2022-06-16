241ffb2ea85842ab4a77095457949a595a2c2864
murtuza <murtuza.creativity@gmail.com>
2022-06-16 12:27:35 +0530

    login implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminRegister from "./pages/AdminRegister";
+import AdminLogin from "./pages/AdminLogin";
 import Home from "./pages/Home";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/registerAdmin" element={<AdminRegister />} />
+          <Route path="/loginAdmin" element={<AdminLogin />} />
         </Routes>
       </BrowserRouter>
     </main>
