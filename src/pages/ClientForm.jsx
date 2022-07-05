1f1077a3daaf4ceba876fba2a9036ee2e4231067
murtuza <murtuza.creativity@gmail.com>
2022-07-05 16:33:50 +0530

    styled client side Form

--- src/pages/ClientForm.jsx
+++ src/pages/ClientForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import '../styles/ClientForm.module.css'
+import styles from '../styles/ClientForm.module.scss'
 const { nanoid } = require('nanoid')
 
 const ClientForm = () => {
@@ -43,23 +43,23 @@ const ClientForm = () => {
                 resType: typeValue
             })
         }
-        
+
         document.querySelectorAll(`.${e.target.className} .questions .question .inputs`).forEach((field) => {
             const questionID = field.dataset.qid;
             const checkboxTexts = []
             document.querySelectorAll(`.input-${questionID} input`).forEach((input, index) => {
-                if(input.type === 'text') {
+                if (input.type === 'text') {
                     pushResponses(input.type, [input.value], questionID)
-                } else if(input.type === 'checkbox') {
-                    if(input.checked === true) {
+                } else if (input.type === 'checkbox') {
+                    if (input.checked === true) {
                         checkboxTexts.push(input.value)
                     }
-                    if(index === (document.querySelectorAll(`.input-${questionID} input`).length - 1)) {
+                    if (index === (document.querySelectorAll(`.input-${questionID} input`).length - 1)) {
                         pushResponses(input.type, checkboxTexts, questionID)
                     }
-                    
-                } else if(input.type === 'radio') {
-                    if(input.checked === true) {
+
+                } else if (input.type === 'radio') {
+                    if (input.checked === true) {
                         pushResponses(input.type, [input.value], questionID)
                     }
                 }
@@ -90,45 +90,49 @@ const ClientForm = () => {
         <>
             {
                 !submitted ?
-                (loading ? 'loading' : (
-                    <form className='formdata' onSubmit={(e) => handleSubmit(e)}>
-                        <h1 className="formTitle">
-                            {formData.title}
-                        </h1>
-                        <h2 className='formDesc'>
-                            {formData.desc}
-                        </h2>
-                        <div className="questions">{
-                            formData.questions.length >= 1 ? (
-                                formData.questions.map((question, index) => {
-                                    return (
-                                        <div key={index} className="question">
-                                            <div className='quesNumber'>Q:- {index + 1}</div>
-                                            <div className="questionText">
-                                                {question.QuestionText}
+                    (loading ? 'loading' : (
+                        <form className={styles.formContent} onSubmit={(e) => handleSubmit(e)}>
+                            <div className={styles.formMetaData}>
+                                <h1 className={styles.formTitle}>
+                                    {formData.title}
+                                </h1>
+                                <h2 className={styles.formDesc}>
+                                    {formData.desc}
+                                </h2>
+                            </div>
+                            <div className={styles.questions}>{
+                                formData.questions.length >= 1 ? (
+                                    formData.questions.map((question, index) => {
+                                        return (
+                                            <div key={index} className={styles.question}>
+                                                <div className={styles.quesNumber}>Q:- {index + 1}</div>
+                                                <div className={styles.questionText}>
+                                                    {question.QuestionText}
+                                                </div>
+                                                <div className={styles.inputContainer}>
+                                                    <div className={`${styles.inputs} ${`inputs input-${question._id}`}`} data-qid={question._id}>
+                                                        {question.Option.length > 0 ? (
+                                                            question.Option.map((option, index) => {
+                                                                return (question.type === 'multipleChoice') ? <label key={index}><input type='checkbox' value={option.OptionText} name={question._id} />{option.OptionText}<br /></label> : <label key={index}><input type='radio' value={option.OptionText} name={question._id} />{option.OptionText}<br /></label>
+                                                            })
+                                                        ) : (
+                                                            question.type === 'text' && <input type='text' name={question._id} placeholder='Type your answer' />
+                                                        )}
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className={`inputs input-${question._id}`} data-qid={question._id}>
-                                                {question.Option.length > 0 ? (
-                                                    question.Option.map((option, index) => {
-                                                        return (question.type === 'multipleChoice') ? <label key={index}><input type='checkbox' value={option.OptionText} name={question._id} />{option.OptionText}<br /></label> : <label key={index}><input type='radio' value={option.OptionText} name={question._id} />{option.OptionText}<br /></label>
-                                                    })
-                                                ) : (
-                                                    question.type === 'text' && <input type='text' name={question._id} placeholder='Type your answer' />
-                                                )}
-                                            </div>
-                                        </div>
-                                    )
-                                })
-                            ) : (
-                                <div>{'No questions'}</div>
-                            )
-                        }</div>
-                        <br />
-                        <button>Submit</button>
-                    </form>
-                )) : (
-                    <h3>Your form has been successfully submitted! Thank You!</h3>
-                )
+                                        )
+                                    })
+                                ) : (
+                                    <p>{'No questions'}</p>
+                                )
+                            }</div>
+
+                            <button className={styles.deleteOrSubmitBtn}>Submit</button>
+                        </form>
+                    )) : (
+                        <div className={styles.formDeletedOrSubmittedMsg}>Your form has been successfully submitted! Thank You!</div>
+                    )
             }
         </>
     )
