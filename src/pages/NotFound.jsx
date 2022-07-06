f0bc671dc860ebed694b970513aefccef74122cc
murtuza <murtuza.creativity@gmail.com>
2022-07-06 12:00:36 +0530

    styled 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import '../styles/NotFound.module.css'
+import styles from '../styles/NotFound.module.scss'
 
 const NotFound = () => {
   return (
-    <>
+    <section className={styles.contain}>
         <h1>404</h1>
         <h2>Page not found</h2>
         <h6>We couldn't find what you were looking for!</h6>
-    </>
+    </section>
   )
 }
 
